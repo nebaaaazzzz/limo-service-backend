5211c542272ff0cbd3f023a68726dc5fc693027a
nebiyu <nebiyuf@gmail.com>
2023-04-28 11:35:46 +0300

    fix: remove mysql migration file

--- build/prisma/migrations/20230417034808_book_model_luggage_and_passenger_to_string/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `Book` MODIFY `phoneNumber` VARCHAR(191) NOT NULL,
-    MODIFY `luggageCount` VARCHAR(191) NOT NULL;
--- build/prisma/migrations/20230425135820_book_status_accepted_completed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [ACCEPTED] on the enum `Book_status` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `Book` MODIFY `status` ENUM('PENDING', 'COMPLETED', 'REJECTED') NOT NULL DEFAULT 'PENDING';
