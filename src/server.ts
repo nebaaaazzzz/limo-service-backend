5804fcb5421f3e5e61b6f3f70daf5fe96d633c69
nebiyu <nebiyuf@gmail.com>
2023-04-28 11:42:16 +0300

    fix: exception handing for user creation

--- src/server.ts
+++ src/server.ts
@@ -12,15 +12,20 @@ import { verifyToken } from "./util/token";
 const PORT = process.env.PORT || 3030;
 
 (async () => {
-  await User.create({
-    data: {
-      email: "neba@gmail.com",
-      lastName: "Daniel",
-      firstName: "Nebiyu",
-      // password: "123456",
-      password: "$2a$10$Bl4Y5US/Gh2bOn6GchivH.GUjKcbEp.h9q8gHaEenpoJ1GMDgLlmi",
-    },
-  });
+  try {
+    await User.create({
+      data: {
+        email: "neba@gmail.com",
+        lastName: "Daniel",
+        firstName: "Nebiyu",
+        // password: "123456",
+        password:
+          "$2a$10$Bl4Y5US/Gh2bOn6GchivH.GUjKcbEp.h9q8gHaEenpoJ1GMDgLlmi",
+      },
+    });
+  } catch (e: any) {
+    console.log(e.message);
+  }
 })();
 const app = express();
 app.use(express.static(path.join(__dirname, "uploads")));
