e8d207e3a2a453e572c18e7ab7e84a3ba70940b5
nebiyu <nebiyuf@gmail.com>
2023-05-07 16:36:13 +0300

    validation schemas fix

--- src/validation_schemas/blog.schema.ts
+++ src/validation_schemas/blog.schema.ts
@@ -1,14 +1,17 @@
 import Joi from "joi";
 const BlogPostschema = Joi.object({
-  title: Joi.string().max(500).required(),
+  title: Joi.string().required(),
   img: Joi.string().required(),
-  content: Joi.string().max(200000).required(),
+  userId: Joi.number(),
+  content: Joi.string().required(),
   // published: Joi.boolean().required(),
 });
 const BlogUpdateschema = Joi.object({
-  title: Joi.string().max(500),
+  title: Joi.string(),
+  userId: Joi.number(),
+
   img: Joi.string(),
-  content: Joi.string().max(200000),
+  content: Joi.string(),
   // published: Joi.boolean(),
 });
 
--- src/validation_schemas/vehicle.schema.ts
+++ src/validation_schemas/vehicle.schema.ts
@@ -1,25 +1,28 @@
 import Joi from "joi";
 import { VehicleType } from "@prisma/client";
 const VehiclePostschema = Joi.object({
-  name: Joi.string().min(3).max(50).required(),
-  model: Joi.string().min(3).max(50).required(),
+  name: Joi.string().min(3).required(),
+  model: Joi.string().min(3).required(),
   img: Joi.string().required(),
-  description: Joi.string().max(20000).required(),
+  description: Joi.string().required(),
   automatic: Joi.number().required(),
   heatedSeat: Joi.number().required(),
   gpsNavigation: Joi.number().required(),
   speed: Joi.number().required().min(0),
   passengerSize: Joi.number().required().min(0),
   pricePerDay: Joi.number().required().min(0),
+  userId: Joi.number(),
   type: Joi.string()
     .required()
     .allow(...Object.values(VehicleType)),
 });
 const VehicleUpdateschema = Joi.object({
-  name: Joi.string().min(3).max(50),
-  model: Joi.string().min(3).max(50),
+  name: Joi.string().min(3),
+  model: Joi.string().min(3),
+  userId: Joi.number(),
+
   img: Joi.string(),
-  description: Joi.string().max(20000).required(),
+  description: Joi.string().required(),
   speed: Joi.number().min(0),
   passengerSize: Joi.number().min(0),
   pricePerDay: Joi.number().min(0),
