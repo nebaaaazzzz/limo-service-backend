bc099f9fe8c7ef3ba570b8d1ba1d49fb6362ee63
Nebiyu <60134585+nebaaaazzzz@users.noreply.github.com>
2023-07-05 19:02:00 +0300

    Update server.js

--- build/server.js
+++ build/server.js
@@ -29,6 +29,7 @@ app.use((0, _cors.default)({
         "https://seattle-limo.onrender.com",
         "http://localhost:3000",
         "127.0.0.1:3000",
+        "https://seattle-limos.onrender.com",
         "http://localhost",
         "127.0.0.1"
     ]
