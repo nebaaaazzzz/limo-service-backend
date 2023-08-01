d9aeafbe1a93a6e739dbf01ebeabd7e1f0b97b24
Bruhtesfa Zelealem <61186942+bruhtesfa-gh@users.noreply.github.com>
2023-08-01 09:34:06 +0300

    add hashing password

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,6 +1,19 @@
 import { User } from "../config/db";
 import { verifyToken } from "./token";
 import { Request, Response, NextFunction } from "express";
+
+import bcrypt from "bcrypt";
+
+export async function hashPassword(password: string) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+export const passwordCompare = async (password: string, hashedPassword: string) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
 export async function isAuth(req: Request, res: Response, next: NextFunction) {
   const decodedToken = await verifyToken(req.cookies.token);
 
