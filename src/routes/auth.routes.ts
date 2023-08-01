020bf7a3ff589ccf5f64df7471396ca342782b8f
Bruhtesfa Zelealem <61186942+bruhtesfa-gh@users.noreply.github.com>
2023-08-01 09:38:25 +0300

    add register route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,75 @@
 import { Router } from "express";
 import passport from "passport";
 import { generateToken } from "../util/token";
+import { User } from "../config/db";
+import { hashPassword, passwordCompare } from "../util/auth";
 const router = Router();
+
+router.post("/register", async function (req, res, next) {
+  // register new user
+  const { email, password, firstName, lastName, img } = req.body;
+  console.log(req.body);
+  try {
+    if (!email || !password || !firstName || !lastName || !img) {
+      return res.status(400).send({
+        data: {
+          message: "all fields are required",
+        },
+      });
+    }
+    let existingUser = await User.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return res.status(400).send({
+        data: {
+          message: "user already exists",
+        },
+      });
+    }
+    const hashedPassword = await hashPassword(password);
+    const user = await User.create({
+      data: {
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        img,
+      },
+    });
+    if (!user) {
+      return res.status(500).send({
+        data: {
+          message: "something went wrong",
+        },
+      });
+    }
+    const token = await generateToken(user);
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        sameSite: "none",
+      })
+      .send({
+        data: {
+          message: "succeeessfuly registered",
+          user
+        },
+      });
+  } catch (err: any) {
+    return res.status(500).send({
+      data: {
+        message: err.message,
+      },
+    });
+  }
+});
+
+
 router.post(
   "/login",
   passport.authenticate("local", {
